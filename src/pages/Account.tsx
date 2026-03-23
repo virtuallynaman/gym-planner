2b2feb468de9794b215576e57e12d7b33bfb9cff
virtuallynaman <jhanaman786@gmail.com>
2026-03-23 22:14:20 +0530

    Add account page

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,9 +1,15 @@
+import { AccountView } from "@neondatabase/neon-js/auth/react";
+import { useParams } from "react-router-dom";
+
 function Account() {
+    const { pathname } = useParams();
     return (
-        <div>
-            
+        <div className="min-h-screen pt-24 pb-12 px-6">
+            <div className="max-w-4xl mx-auto">
+                <AccountView pathname={pathname} />
+            </div>
         </div>
-    )
+    );
 }
 
-export default Account
+export default Account;
